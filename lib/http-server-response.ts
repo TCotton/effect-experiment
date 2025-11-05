2bcfd5a49fcfe08353a3790a9b188b28bcfcf3ca
TCotton <me@andywalpole.me>
2025-11-05 12:55:38 +0000

    added http-server-response to codebase

---
+++ lib/http-server-response.ts
@@ -0,0 +1,28 @@
+export type HTTPServerResponse = {
+    readonly status: number;
+    readonly headers?: Record<string, string | string[] | undefined>
+    readonly body: string | Uint8Array
+}
+
+export const json = (
+    status: number,
+    body: string
+):HTTPServerResponse => ({
+    status,
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body
+})
+
+export const empty = (
+    status: number = 204
+): HTTPServerResponse => ({
+    status,
+    body: ""
+})
+
+export const internalError = () => HTTPServerResponse => ({
+    status: 500,
+    body: "Internal Server Error"
+})
